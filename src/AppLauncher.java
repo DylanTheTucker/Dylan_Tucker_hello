efbf44df678e090b1ccdb735f5aa19fb0ec22aa4
Kenneth Jamieson <kjamieson@murraystate.edu>
2025-08-22 20:17:18 -0500

    Adding Part 3
    This is everything but the last question, Need to add exception handling

--- src/AppLauncher.java
+++ src/AppLauncher.java
@@ -1,11 +1,27 @@
+import java.util.Scanner;
+
 public class AppLauncher {
     //Main Body
     public static void main(String[] args) 
     { 
-        //Construct HelloWorld Object
-        HelloWorld student = new HelloWorld("Dylan", 20);
-        HelloWorld THEjason = new HelloWorld("Dylan");
+        //Allows input for name and age
+        Scanner sc = new Scanner(System.in);
 
+        System.out.println("Name?");
+        String name = sc.next();
+        System.out.println("age?");
+        int age = sc.nextInt();
+
+        if(age < 0)
+        {
+            System.out.println("Error");
+        }
+
+        //Construct HelloWorld Object
+        HelloWorld student = new HelloWorld(name, age);
+        HelloWorld THEjason = new HelloWorld(name);
+
+        //Calling the Methods
         student.introduce();
         THEjason.greet(); 
         THEjason.greet("How are you today?");
