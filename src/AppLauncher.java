df1700b627bbd840e28a83ba37fd4ecd318a3ef7
Kenneth Jamieson <kjamieson@murraystate.edu>
2025-08-25 08:54:02 -0500

    Finished Part 3
    Last part of Part 3 everything is running, added the try and catch.

--- src/AppLauncher.java
+++ src/AppLauncher.java
@@ -4,26 +4,39 @@ public class AppLauncher {
     //Main Body
     public static void main(String[] args) 
     { 
-        //Allows input for name and age
-        Scanner sc = new Scanner(System.in);
+        try {
+            
+            //Allows input for name and age
+            Scanner sc = new Scanner(System.in);
 
-        System.out.println("Name?");
-        String name = sc.next();
-        System.out.println("age?");
-        int age = sc.nextInt();
+            System.out.println("Name?");
+            String name = sc.next();
 
-        if(age < 0)
-        {
-            System.out.println("Error");
+            //incase you try to put empty name
+            if(name.isEmpty())
+            {
+                System.out.println("Invaild");
+                System.exit(0);
+            }
+            System.out.println("age?");
+            int age = sc.nextInt();
+            //Incase you try to put a number less then 0
+            if(age < 0)
+            {
+                System.out.println("Invaild");
+                System.exit(0);
+            }
+            //Construct HelloWorld Object
+            HelloWorld student = new HelloWorld(name, age);
+            HelloWorld THEjason = new HelloWorld(name);
+
+            //Calling the Methods
+            student.introduce();
+            THEjason.greet(); 
+            THEjason.greet("How are you today?");
+        } 
+         catch (Exception e) {
+            System.out.println("Invaild");
         }
-
-        //Construct HelloWorld Object
-        HelloWorld student = new HelloWorld(name, age);
-        HelloWorld THEjason = new HelloWorld(name);
-
-        //Calling the Methods
-        student.introduce();
-        THEjason.greet(); 
-        THEjason.greet("How are you today?");
     } 
 }
